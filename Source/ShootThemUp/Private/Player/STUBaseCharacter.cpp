0b291f27c86c19a47e2c122a6bea0bc2d713d1cd
Yura Molotov <yuramltv@gmail.com>
2025-08-25 08:01:34 -0700

    switch to spectator pawn on death

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -8,6 +8,7 @@
 #include "Components/STU_CharacterMovementComponent.h"
 #include "Components/STUHealthComponent.h"
 #include "Components/TextRenderComponent.h"
+#include "GameFramework/Controller.h"
 
 DEFINE_LOG_CATEGORY_STATIC(BaseCharacterLog, All, All);
 
@@ -132,10 +133,12 @@ void ASTUBaseCharacter::OnDeath()
     UE_LOG(BaseCharacterLog, Display, TEXT("Player %s is dead"), *GetName());
 
     PlayAnimMontage(DeathAnimMontage);
-
     GetCharacterMovement()->DisableMovement();
-
     SetLifeSpan(5.0f);
+    if(Controller)
+    {
+        Controller->ChangeState(NAME_Spectating);
+    }
 }
 void ASTUBaseCharacter::OnHealthChanged(float Health)
 {
